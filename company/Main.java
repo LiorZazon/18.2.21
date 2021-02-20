01b979f66286e3d2c4b79ce46a232e9197e6b95b
LiorZazon <75090709+LiorZazon@users.noreply.github.com>
2021-02-20 23:10:24 +0200

    Add files via upload

---
+++ company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    Triangle t1 = new Triangle(6.4, 9.3, 5.2, 4.3);
+    TriangleEqualSides t2 = new TriangleEqualSides(6.2,3.5,6.4);
+    TriangleWith90 t3 = new TriangleWith90(1.2,4.5,6.3);
+
+    }
+}
---
+++ company/TriangleEqualSides.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class TriangleEqualSides extends Triangle {
+
+    public TriangleEqualSides(double m_a, double m_c, double m_h) {
+        super(m_a, m_a, m_c, m_h);
+    }
+    @Override
+    public double getHekef() {
+        return m_a * 2 + m_c;
+
+    }
+}
